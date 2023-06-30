5cfcc48975b652a1c2f7dbffa98c8ce18f148a35
777Afanas <serhii2111@yahoo.com>
2023-06-30 19:46:28 +0300

    Update_01_color-swicher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+
+const startBtn = document.querySelector('[data-start');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+const startClick = () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);     
+    startBtn.setAttribute("disabled", "disabled");
+}
+
+startBtn.addEventListener("click", startClick);
+
+const stopClick = () => {
+    clearInterval(timerId);
+    startBtn.removeAttribute("disabled");
+}
+
+stopBtn.addEventListener("click", stopClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
