8c64418a6f359ba15a746c01d0f1e6799e8f9167
777Afanas <serhii2111@yahoo.com>
2023-07-04 13:10:18 +0000

    Deploying to gh-pages from @ 777Afanas/double--js-hw-09@03de965ceeb41cead30da146fe0652ce99294f79 🚀

--- 01-color-switcher.f4dfc454.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start"),e=document.querySelector("[data-stop]");let a=null;t.addEventListener("click",(()=>{a=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.setAttribute("disabled","disabled")}));e.addEventListener("click",(()=>{clearInterval(a),t.removeAttribute("disabled")}));
-//# sourceMappingURL=01-color-switcher.f4dfc454.js.map
