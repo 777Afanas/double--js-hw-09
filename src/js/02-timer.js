e3a303040044e901cab9db9a5a7dc8d3f4eefc6a
Serhii Yatsenko <serhii2111@yahoo.com>
2023-07-07 00:06:45 +0300

    update 02-timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,11 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
 
+const test = document.querySelector('#datetime-picker');
+
+
+// flatpickr(test, options);
+
 const options = {
     enableTime: true,
     time_24hr: true,
@@ -11,5 +16,28 @@ const options = {
         console.log(selectedDates[0]);
     },
 };
+flatpickr(test, options);
 
-flatpickr("#datetime-picker", options);
+
+
+
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
